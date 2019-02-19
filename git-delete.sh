bd70b5a6fb11faef4ed7cc08a9d3690a0a9e772e
Carlo Revelli <carlo.revelli@berkeley.edu>
2019-02-19 14:46:06 -0800

    simplify

--- git-delete.sh
+++ git-delete.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-for branch in $(git branch | sed 's/^*//'); do
-  current_branch=$(git branch | grep -e '^*')
-  if [[ "* $branch" == "$current_branch" ]]; then
-    printf "Skipping $branch\n"
-    continue
-  fi
+for branch in $(git branch | grep -v '^*'); do
   read -p "Would you like to delete $branch? (y/n)" -N 1 answer
   if [[ "$answer" == "y" ]]; then
     git branch -D "$branch"
